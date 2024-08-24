a1058b38ab7536be7825853f1613fbde8d56f505
prasad20250 <prasadgaikwad9112@gmail.com>
2024-08-24 14:19:28 +0530

    Add files via upload

---
+++ string manupaltion.cpp.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include<string>
+using namespace std;
+
+int main() {
+  string str = "Prasad";
+  
+  //concatenation
+  string greeting = str + " Gaikwad";
+  cout<< "concatenation:"<< greeting << endl ;
+  
+  //Substring
+  string part = greeting.substr (7,8);// Extracts, "World"
+  cout<< "Substring :" << part << endl ;
+  
+  //find
+  size_t pos = greeting.find("Gaikwad");
+  cout<<"find: 'Gaikwad' found at position " <<pos<<endl; 
+  
+  
+  //Replace 
+  string replaced = greeting;
+  replaced.replace(pos, 7 , "sir");
+  cout<<"replace:"<<replaced << endl ;
+  
+  //Erase
+  string erased = greeting;
+  erased.erase(6,9);// removes " beautiful"
+  cout<<"erase:"<<erased<<endl;
+  
+  // To uppercase 
+  string uppercase = greeting ;
+  for (char &C : uppercase)C = toupper(C);
+  cout<<"to uppercase:"<<uppercase<<endl;
+  
+  // To lowercase 
+  string lowercase = greeting ;
+  for (char &C : lowercase)C = tolower(C);
+  cout<<"to lowercase:"<<lowercase<<endl;
+  
+  
+  
+  
+  
+}
